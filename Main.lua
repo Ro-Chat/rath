4dc0ba4189b5ec8e74ba94feb4d6247ad2fc1b74
Ro-Chat <121181030+Ro-Chat@users.noreply.github.com>
2023-01-29 16:36:21 -0800

    Update and rename Init.lua to Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+local Status, Release = pcall(function() return debug.getinfo(4) end)
+Release = Status and Release
+
+local Script = not Release and readfile("Rath/Init.lua") or game:HttpGet("https://raw.githubusercontent.com/Ro-Chat/rath/main/Init.lua")
+
+loadstring(Script)()(Release)
