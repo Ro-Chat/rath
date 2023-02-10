ac2cbbfb81cf85947f804dd9f6b62526b9987df1
Ro-Chat <121181030+Ro-Chat@users.noreply.github.com>
2023-02-10 04:47:23 -0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,6 +4,10 @@
 local Status, Release = pcall(function() return debug.getinfo(4) end)
 Release = Status and Release
 
-local Script = not Release and readfile("Rath/Init.lua") or game:HttpGet("https://raw.githubusercontent.com/Ro-Chat/rath/main/Init.lua")
+local Script = not Release and readfile("rath/Init.lua") or game:HttpGet("https://raw.githubusercontent.com/Ro-Chat/rath/main/Init.lua")
 
-loadstring(Script)()(Release)
+-- Import
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ro-Chat/Import/main/Main.lua"))()(Release, "Ro-Chat/rath", "main")
+
+-- Main
+loadstring(Script)()
